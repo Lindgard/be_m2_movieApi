3e80f32e2e53498b5f45920f7e7e1ea0fddfcf54
ALP <a.b.lindgard@gmail.com>
2026-05-13 12:51:16 +0200

    added CRUD methods for movies

--- MovieService.Api/Services/MovieService.cs
+++ MovieService.Api/Services/MovieService.cs
@@ -1,4 +1,5 @@
 using MovieService.Api.Data;
+using MovieService.Api.Models;
 
 namespace MovieService.Api.Services;
 
@@ -9,5 +10,40 @@ public class MovieService
     public MovieService(MovieDbContext dbContext)
     {
         _dbContext = dbContext;
+    }
+
+    public List<string> GetMovies()
+    {
+        var query = from m in _dbContext.Set<Movie>()
+                    select m.Title;
+        return query.ToList();
+    }
+
+    public Movie AddMovie(string title)
+    {
+        var movie = new Movie { Title = title };
+        _dbContext.Set<Movie>().Add(movie);
+        _dbContext.SaveChanges();
+        return movie;
+    }
+
+    public Movie? GetMovieById(int id)
+    {
+        return _dbContext.Set<Movie>().Find(id);
+    }
+
+    public void DeleteMovie(int id)
+    {
+        var movie = new Movie { Id = id };
+        _dbContext.Set<Movie>().Remove(movie);
+        _dbContext.SaveChanges();
+    }
+
+    public Movie UpdateMovie(int id, string title)
+    {
+        var movie = new Movie { Id = id, Title = title };
+        _dbContext.Set<Movie>().Update(movie);
+        _dbContext.SaveChanges();
+        return movie;
     }
 }
