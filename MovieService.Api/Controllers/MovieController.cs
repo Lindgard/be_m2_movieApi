7034d55d81c858d70486ab596cf04392dfa6d66d
ALP <a.b.lindgard@gmail.com>
2026-05-13 12:00:13 +0200

    added controller and entity framework for database handling

---
+++ MovieService.Api/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieService.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MovieController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetMovies()
+    {
+        var movies = new List<string>
+        {
+            "Star Wars",
+            "The Lord of the Rings",
+            "The Matrix",
+            "Inception",
+            "The Dark Knight"
+        };
+        return Ok(movies);
+    }
+}
